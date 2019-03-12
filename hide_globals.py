d72ad8aef319daca99b39592ae1b3ee731f51bfd
chengwill97 <chengwill97@gmail.com>
2019-03-12 09:37:30 -0400

    Create decorator to hide globals
    - decorator includes builtins and imports

---
+++ hide_globals.py
@@ -0,0 +1,22 @@
+import types
+
+def noglobals(f):
+    inner_globals = dict()
+    inner_globals['__builtins__'] = globals().get('__builtins__')
+    inner_globals['__import__'] = globals().get('__import__')
+    return types.FunctionType(f.__code__, inner_globals)
+
+import os
+
+@noglobals
+def test1():
+    try:
+        print(os.__name__)
+    except NameError:
+        print('os module can\'t be imported from globals')
+        print('importing inside')
+        import os
+        print(os.__name__)
+
+test1()
+print(globals())
